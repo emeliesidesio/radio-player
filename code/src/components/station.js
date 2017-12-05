954661963140693d8111431a7c91488c74f62521
Emelie Sidesiö <esidesio@gmail.com>
2017-12-05 15:05:31 +0100

    background color + css styling

--- code/src/components/station.js
+++ code/src/components/station.js
@@ -3,13 +3,16 @@ import React from "react"
 class Station extends React.Component {
   render() {
     return (
-      <div>
-        <div>{this.props.name}</div>
-        <img src={this.props.image} alt="text" />
-        <div>{this.props.color}</div>
-        <audio controls src={this.props.audio} type="audio/mpeg">
-          <track kind="captions" />
-        </audio>
+      <div className="radio-box">
+        <div className="image-box">
+          <img src={this.props.image} alt="text" />
+        </div>
+        <div className="content-box" style={{ backgroundColor: `#${this.props.color}` }}>
+          <h1>{this.props.name}</h1>
+          <audio controls src={this.props.audio} type="audio/mpeg">
+            <track kind="captions" />
+          </audio>
+        </div>
       </div>
     )
   }
